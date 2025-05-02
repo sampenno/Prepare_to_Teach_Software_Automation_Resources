230125b469aab23b7e3253098f2704d3a2f23451
Ben Jones <benjamin.jones21@det.nsw.edu.au>
2025-05-02 23:36:55 +0000

    improve cost function visual

--- Week_1_-_Linear_Regression/utils_uni.py
+++ Week_1_-_Linear_Regression/utils_uni.py
@@ -60,34 +60,34 @@ def mk_cost_lines(x,y,w,b, ax):
 
 def plt_intuition(x_train, y_train):
 
-    w_range = np.array([200-200,200+200])
+    m_range = np.array([200-200,200+200])
     tmp_b = 100
 
-    w_array = np.arange(*w_range, 5)
-    cost = np.zeros_like(w_array)
-    for i in range(len(w_array)):
-        tmp_w = w_array[i]
-        cost[i] = compute_cost(x_train, y_train, tmp_w, tmp_b)
+    m_array = np.arange(*m_range, 5)
+    cost = np.zeros_like(m_array)
+    for i in range(len(m_array)):
+        tmp_m = m_array[i]
+        cost[i] = compute_cost(x_train, y_train, tmp_m, tmp_b)
 
-    @interact(w=(*w_range,10),continuous_update=False)
-    def func( w=150):
-        f_wb = np.dot(x_train, w) + tmp_b
+    @interact(mw=(*m_range,10),continuous_update=False)
+    def func( m=150):
+        f_mb = np.dot(x_train, m) + tmp_b
         
         fig, ax = plt.subplots(1, 2, constrained_layout=True, figsize=(8,4))
         
         fig.canvas.toolbar_position = 'bottom'
 
-        mk_cost_lines(x_train, y_train, w, tmp_b, ax[0])
-        plt_x(x_train, y_train, f_wb=f_wb, ax=ax[0])
+        mk_cost_lines(x_train, y_train, m, tmp_b, ax[0])
+        plt_x(x_train, y_train, f_wb=f_mb, ax=ax[0])
 
-        ax[1].plot(w_array, cost)
-        cur_cost = compute_cost(x_train, y_train, w, tmp_b)
-        ax[1].scatter(w,cur_cost, s=100, color=dldarkred, zorder= 10, label= f"cost at w={w}")
-        ax[1].hlines(cur_cost, ax[1].get_xlim()[0],w, lw=4, color=dlpurple, ls='dotted')
-        ax[1].vlines(w, ax[1].get_ylim()[0],cur_cost, lw=4, color=dlpurple, ls='dotted')
-        ax[1].set_title("Cost vs. w, (b fixed at 100)")
+        ax[1].plot(m_array, cost)
+        cur_cost = compute_cost(x_train, y_train, m, tmp_b)
+        ax[1].scatter(m,cur_cost, s=100, color=dldarkred, zorder= 10, label= f"cost at m={m}")
+        ax[1].hlines(cur_cost, ax[1].get_xlim()[0],m, lw=4, color=dlpurple, ls='dotted')
+        ax[1].vlines(m, ax[1].get_ylim()[0],cur_cost, lw=4, color=dlpurple, ls='dotted')
+        ax[1].set_title("Cost vs. m, (b fixed at 100)")
         ax[1].set_ylabel('Cost')
-        ax[1].set_xlabel('w')
+        ax[1].set_xlabel('m')
         ax[1].legend(loc='upper center')
         fig.suptitle(f"Minimize Cost: Current Cost = {cur_cost:0.0f}", fontsize=12)
         plt.show()
